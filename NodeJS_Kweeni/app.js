e33a8e741c9276552d2e59b3c2aee000075458cd
RafaelDesignBE <thegoldenboxmail@gmail.com>
2018-03-20 16:07:04 +0100

    Change port settings

--- NodeJS_Kweeni/app.js
+++ NodeJS_Kweeni/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const PORT = process.env.PORT || 5000
 
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
@@ -8,5 +9,4 @@ app.use(bodyParser.json());
 
 
 app.use("/kweeni", require('./routes/kweeni'));
-
-app.listen(3000, () => console.log('Running...'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
